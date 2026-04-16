cd2f47efeaeca87bbe29947b747d2c5469582ecd
Lyory <trongquan29605@gmail.com>
2026-04-16 23:35:06 +0700

    Initial commit: Upload robot ROS 2 package

---
+++ launch/display.launch.py
@@ -0,0 +1,106 @@
+import os
+import xacro
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    # 1. Khai báo các đường dẫn
+    pkg_share = get_package_share_directory('robot') # Tên package của bạn
+    
+    # Lấy đường dẫn tới package turtlebot3_gazebo
+    pkg_tb3_gazebo = get_package_share_directory('turtlebot3_gazebo')
+    
+    # Chọn map Waffle (turtlebot3_world.world là map kinh điển có các cột)
+    world_path = os.path.join(pkg_tb3_gazebo, 'worlds', 'turtlebot3_world.world')
+    
+    # Đường dẫn để Gazebo tìm thấy mesh của robot bạn
+    workspace_models_path = os.path.join(pkg_share, '..')
+
+    # 2. Xử lý file URDF
+    xacro_file = os.path.join(pkg_share, 'urdf', 'robot.urdf')
+    robot_desc = xacro.process_file(xacro_file).toxml()
+
+    # 3. Thiết lập biến môi trường GAZEBO_MODEL_PATH
+    # Thêm cả đường dẫn model của TB3 để Gazebo load được các cột, tường trong map
+    set_gazebo_model_path = SetEnvironmentVariable(
+        name='GAZEBO_MODEL_PATH',
+        value=[
+            os.environ.get('GAZEBO_MODEL_PATH', ''), 
+            ':', workspace_models_path,
+            ':', os.path.join(pkg_tb3_gazebo, 'models')
+        ]
+    )
+
+    # 4. Node Robot State Publisher
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': robot_desc, 'use_sim_time': True}]
+    )
+
+    # 5. Mở Gazebo và Nạp Map
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
+        ),
+        launch_arguments={'world': world_path}.items()
+    )
+
+    # 6. Node Spawn Robot
+    # Chỉnh tọa độ spawn (x: -2.0) để robot không xuất hiện đè lên cái cột ở tâm map
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-topic', 'robot_description', 
+            '-entity', 'my_robot',
+            '-x', '-2.0', '-y', '-0.5', '-z', '0.01'
+        ],
+        output='screen'
+    )
+
+   # Khai báo đường dẫn tới file cấu hình RViz của bạn
+    rviz_config_path = os.path.join(pkg_share, 'rviz', 'robot.rviz')
+
+    # Node RViz2 
+    rviz2 = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        # Thêm arguments '-d' để nạp file cấu hình
+        arguments=['-d', rviz_config_path], 
+        parameters=[{'use_sim_time': True}]
+    )
+
+    # 8. Controller Spawners (Kích hoạt bộ điều khiển)
+    load_joint_state_broadcaster = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+    )
+
+    load_joint_position_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_position_controller"],
+    )
+
+   # load_wheel_velocity_controller = Node(
+    #    package="controller_manager",
+     #   executable="spawner",
+      #  arguments=["wheel_velocity_controller"],
+    #)
+
+    return LaunchDescription([
+        set_gazebo_model_path,
+        robot_state_publisher,
+        gazebo,
+        spawn_entity,
+        rviz2,
+        load_joint_state_broadcaster,
+        load_joint_position_controller,
+    ])
---
+++ scripts/arm_controller.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float64MultiArray
+import time
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = rclpy.create_node('arm_sequential_controller')
+    
+    publisher = node.create_publisher(
+        Float64MultiArray, 
+        '/joint_position_controller/commands', 
+        10)
+    
+    # trang thai hien tai cua robot
+    current_angle = 0.0
+    current_height = 0.0
+
+
+    try:
+        while rclpy.ok():
+            user_input = input("Nhập lệnh (Xoay Nâng): ")
+            
+            if user_input.lower() == 'q':
+                break
+                
+            parts = user_input.split()
+            if len(parts) != 2:
+                print("Error\n")
+                continue
+                
+            try:
+                target_angle = float(parts[0])
+                target_height = float(parts[1])
+                # Xoay
+                print(f"Đang xoay đến góc {target_angle} rad...")
+                msg = Float64MultiArray()
+                msg.data = [target_angle, current_height] # Gửi góc mới, độ cao cũ
+                publisher.publish(msg)
+                
+                # Tạm dừng chương trình để chờ mô tơ trong Gazebo xoay xong
+                time.sleep(2.0) 
+                # Điều khiển tịnh tiến
+                print(f"Đang tịnh tiến đến độ cao {target_height} m...")
+                msg.data = [target_angle, target_height] # Gửi góc MỚI, độ cao MỚI
+                publisher.publish(msg)
+                
+                # Tạm dừng 2 giây chờ nâng xong
+                time.sleep(2.0)
+                
+                print("Finished\n")
+                
+               # Cập nhật vị trí
+                current_angle = target_angle
+                current_height = target_height
+                
+            except ValueError:
+                print("Error\n")
+
+    except KeyboardInterrupt:
+        node.get_logger().info('shutting down arm controller node...')
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
